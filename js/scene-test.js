454b7229335b13e06308ebf64b0a3fb3707816c8
Steven Cobb <stevecobb91@gmail.com>
2017-04-24 16:31:41 -0400

    realtime vertex displacement ex

--- js/scene-test.js
+++ js/scene-test.js
@@ -1,125 +1,268 @@
-/*** SCENE JS ***/
+/*
+*** SCENE JS ***
+*/
 
-var clock;
-clock = new THREE.Clock();
-var elapsed = clock.getElapsedTime();
+//#pragma glslify: pnoise3 = require(glsl-noise/periodic/3d)
 
-AFRAME.registerComponent('fire', {
-  schema: {
-    width: {type: 'number', default: 1},
-    height: {type: 'number', default: 1},
-    depth: {type: 'number', default: 1},
-    color: {type: 'color', default: '#AAA'}
-  },
+
+//const glsl = require('glslify');
+
+AFRAME.registerComponent('material-displacement', {
+  /**
+   * Creates a new THREE.ShaderMaterial using the two shaders defined
+   * in vertex.glsl and fragment.glsl.
+   */
   init: function () {
-    var data = this.data;
-    var el = this.el;
+    this.material  = new THREE.ShaderMaterial({
+      uniforms: {time: { value: 0.0 }},
+      vertexShader: this.vertexShader,
+      fragmentShader: this.fragmentShader,
+    });
+    this.el.addEventListener('model-loaded', () => this.update());
+  },
+
+  /**
+   * Apply the material to the current entity.
+   */
+  update: function () {
+    const mesh = this.el.getObject3D('mesh');
+    if (mesh) {
+      mesh.material = this.material;
+    }
+  },
+
+  /**
+   * On each frame, update the 'time' uniform in the shaders.
+   */
+  tick: function (t) {
+    this.material.uniforms.time.value = t / 1000;
+  },
+
+  vertexShader: [
+    "varying float noise;",
+    "void main() {",
+      "vec3 color = vec3(1. - 2. * noise);",
+      "gl_FragColor = vec4( color.rgb, 1.0 );",
+    "}"
+  ].join('\n'),
+
+  fragmentShader: [
+    "float pnoise3(vec3 P, vec3 rep) {",
+      "vec3 mod289(vec3 x)",
+      "{",
+        "return x - floor(x * (1.0 / 289.0)) * 289.0;",
+      "}",
+      "vec4 mod289(vec4 x)",
+      "{",
+        "return x - floor(x * (1.0 / 289.0)) * 289.0;",
+      "}",
+      "vec4 permute(vec4 x)",
+      "{",
+        "return mod289(((x*34.0)+1.0)*x);",
+      "}",
+      "vec4 taylorInvSqrt(vec4 r)",
+      "{",
+        "return 1.79284291400159 - 0.85373472095314 * r;",
+      "}",
+      "vec3 fade(vec3 t) {",
+        "return t*t*t*(t*(t*6.0-15.0)+10.0);",
+      "}",
+      // Classic Perlin noise, periodic variant
+      "float pnoise(vec3 P, vec3 rep)",
+      "{",
+        "vec3 Pi0 = mod(floor(P), rep); // Integer part, modulo period",
+        "vec3 Pi1 = mod(Pi0 + vec3(1.0), rep); // Integer part + 1, mod period",
+        "Pi0 = mod289(Pi0);",
+        "Pi1 = mod289(Pi1);",
+        "vec3 Pf0 = fract(P); // Fractional part for interpolation",
+        "vec3 Pf1 = Pf0 - vec3(1.0); // Fractional part - 1.0",
+        "vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);",
+        "vec4 iy = vec4(Pi0.yy, Pi1.yy);",
+        "vec4 iz0 = Pi0.zzzz;",
+        "vec4 iz1 = Pi1.zzzz;",
+
+        "vec4 ixy = permute(permute(ix) + iy);",
+        "vec4 ixy0 = permute(ixy + iz0);",
+        "vec4 ixy1 = permute(ixy + iz1);",
+
+        "vec4 gx0 = ixy0 * (1.0 / 7.0);",
+        "vec4 gy0 = fract(floor(gx0) * (1.0 / 7.0)) - 0.5;",
+        "gx0 = fract(gx0);",
+        "vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);",
+        "vec4 sz0 = step(gz0, vec4(0.0));",
+        "gx0 -= sz0 * (step(0.0, gx0) - 0.5);",
+        "gy0 -= sz0 * (step(0.0, gy0) - 0.5);",
+
+        "vec4 gx1 = ixy1 * (1.0 / 7.0);",
+        "vec4 gy1 = fract(floor(gx1) * (1.0 / 7.0)) - 0.5;",
+        "gx1 = fract(gx1);",
+        "vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);",
+        "vec4 sz1 = step(gz1, vec4(0.0));",
+        "gx1 -= sz1 * (step(0.0, gx1) - 0.5);",
+        "gy1 -= sz1 * (step(0.0, gy1) - 0.5);",
+
+        "vec3 g000 = vec3(gx0.x,gy0.x,gz0.x);",
+        "vec3 g100 = vec3(gx0.y,gy0.y,gz0.y);",
+        "vec3 g010 = vec3(gx0.z,gy0.z,gz0.z);",
+        "vec3 g110 = vec3(gx0.w,gy0.w,gz0.w);",
+        "vec3 g001 = vec3(gx1.x,gy1.x,gz1.x);",
+        "vec3 g101 = vec3(gx1.y,gy1.y,gz1.y);",
+        "vec3 g011 = vec3(gx1.z,gy1.z,gz1.z);",
+        "vec3 g111 = vec3(gx1.w,gy1.w,gz1.w);",
+
+        "vec4 norm0 = taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));",
+        "g000 *= norm0.x;",
+        "g010 *= norm0.y;",
+        "g100 *= norm0.z;",
+        "g110 *= norm0.w;",
+        "vec4 norm1 = taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));",
+        "g001 *= norm1.x;",
+        "g011 *= norm1.y;",
+        "g101 *= norm1.z;",
+        "g111 *= norm1.w;",
+
+        "float n000 = dot(g000, Pf0);",
+        "float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));",
+        "float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));",
+        "float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));",
+        "float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));",
+        "float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));",
+        "float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));",
+        "float n111 = dot(g111, Pf1);",
+
+        "vec3 fade_xyz = fade(Pf0);",
+        "vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);",
+        "vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);",
+        "float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x);",
+        "return 2.2 * n_xyz;",
+      "}",
+    "}",
+    "varying float noise;",
+    "uniform float time;",
+    "float turbulence( vec3 p ) {",
+      "float w = 100.0;",
+      "float t = -.5;",
+      "for (float f = 1.0 ; f <= 10.0 ; f++ ){",
+        "float power = pow( 2.0, f );",
+        "t += abs( pnoise3( vec3( power * p ), vec3( 10.0, 10.0, 10.0 ) ) / power );",
+      "}",
+      "return t;",
+    "}",
+    "void main() {",
+      "noise = 10.0 *  -.10 * turbulence( .5 * normal + time / 3.0 );",
+      "float b = 5.0 * pnoise3( 0.05 * position, vec3( 100.0 ) );",
+      "float displacement = (- 10. * noise + b) / 50.0;",
+
+      "vec3 newPosition = position + normal * displacement;",
+      "gl_Position = projectionMatrix * modelViewMatrix * vec4( newPosition, 1.0 );",
+    "}",
+  ].join('\n'),
+})
+
+/*** THREE.Fire: https://github.com/mattatz/THREE.Fire ***/
+AFRAME.registerComponent('fire', {
+  schema: {color: {type: 'color'}},
+  init: function () {
+    const data = this.data,
+          mesh = this.el.getObject3D('mesh');
+
+    this.cameraPosition = new THREE.Vector3();
 
     var loader = new THREE.TextureLoader();
     this.tex = loader.load( '/js/lib/three.fire/Fire.png' );
-
     this.tex.magFilter = this.tex.minFilter = THREE.LinearFilter;
     this.tex.wrapS = THREE.wrapT = THREE.ClampToEdgeWrapping;
 
-    this.geometry = new THREE.BoxGeometry( 1.0, 1.0, 1.0 );
-
     this.material = new THREE.ShaderMaterial( {
-      defines         : THREE.FireShader.defines,
-      uniforms        : THREE.UniformsUtils.clone( THREE.FireShader.uniforms ),
-      vertexShader    : THREE.FireShader.vertexShader,
-      fragmentShader  : THREE.FireShader.fragmentShader,
-      transparent     : true,
-      depthWrite      : false,
-      depthTest       : false
+      defines: {
+        "ITERATIONS"    : "20",
+        "OCTIVES"       : "3"
+      },
+      uniforms: {
+        "fireTex"         : { type : "t",     value : null },
+        "color"           : { type : "c",     value : null },
+        "time"            : { type : "f",     value : 0.0 },
+        "seed"            : { type : "f",     value : 0.0 },
+        "invModelMatrix"  : { type : "m4",    value : null },
+        "scale"           : { type : "v3",    value : null },
+        "noiseScale"      : { type : "v4",    value : new THREE.Vector4(1, 2, 1, 0.3) },
+        "magnitude"       : { type : "f",     value : 1.3 },
+        "lacunarity"      : { type : "f",     value : 2.0 },
+        "gain"            : { type : "f",     value : 0.5 },
+        "vCameraPosition" : { value: this.cameraPosition },
+      },
+      vertexShader: this.vertexShader,
+      fragmentShader: this.fragmentShader,
+      transparent: true,
+      depthWrite: false,
+      depthTest: false
     } );
+
     this.material.uniforms.fireTex.value = this.tex;
     this.material.uniforms.color.value = new THREE.Color( 0xeeeeee );
     this.material.uniforms.invModelMatrix.value = new THREE.Matrix4();
     this.material.uniforms.scale.value = new THREE.Vector3( 1, 1, 1 );
-    //this.material.uniforms.scale.value = this.el.getAttribute('scale');
     this.material.uniforms.seed.value = Math.random() * 19.19;
 
-    this.fire = new THREE.Mesh(this.geometry, this.material);
-    this.fire.frustumCulled = false;
+    //this.fire = new THREE.Mesh(this.geometry, this.material);
+    //this.fire.frustumCulled = false;
     
+    //console.log(this.fire);
+    //el.setObject3D('fire-mesh', this.fire);
+
+    //this.applyToMesh();
+    //this.el.addEventListener('model-loaded', () => this.applyToMesh());
+
     var wireframeMat = new THREE.MeshBasicMaterial({
         color : new THREE.Color(0xffffff),
         wireframe : true
     });
-    var wireframe = new THREE.Mesh(this.fire.geometry, wireframeMat.clone());
-    this.fire.add(wireframe);
-    wireframe.visible = true;
+    var wireframe = new THREE.Mesh(mesh.geometry, wireframeMat.clone());
+    mesh.add(wireframe);
+    //wireframe.visible = true;
 
-    //this.fire.scale.set(3, 3, 3);
-    
-    console.log(this.fire);
-    el.setObject3D('fire-mesh', this.fire);
-    
+    mesh.material = this.material;
+    mesh.position.set(0.5, 0, 0);
   },
-  update: function (oldData) {
+  update: function (data) {
+    mesh = this.el.getObject3D('mesh');
+    //this.material.src = data.src;
+    //this.material.uniforms.scale.value = this.scale;
 
+    this.el.setAttribute('position', {x: 0.5, y: 0, z: 0});
+    //mesh.material = this.material;
+    this.applyToMesh();
+  },
+  applyToMesh: function() {
+    const mesh = this.el.getObject3D('mesh');
+    console.log(mesh);
+
+    mesh.position.set(0.5, 0, 0);
+    console.log(mesh.position);
+    if (mesh) {
+      var wireframeMat = new THREE.MeshBasicMaterial({
+          color : new THREE.Color(0xffffff),
+          wireframe : true
+      });
+      var wireframe = new THREE.Mesh(mesh.geometry, wireframeMat.clone());
+      mesh.add(wireframe);
+      //wireframe.visible = true;
+
+      mesh.material = this.material;
+    }
   },
   tick: function (time, delta) {
-    elapsed = clock.getElapsedTime();
-
-    var invModelMatrix = this.material.uniforms.invModelMatrix.value;
-    //console.log(invModelMatrix);
-
-    this.fire.updateMatrix();
-    invModelMatrix.getInverse( this.fire.matrix );
-
-    this.material.uniforms.time.value = elapsed; //always needed for flame flicker
-
-    this.material.uniforms.invModelMatrix.value = invModelMatrix;
-    
-    //this.material.uniforms.scale.value.set(3, 3, 3);
-
-    //this.material.uniforms.scale.value = this.el.getAttribute('scale');
-    this.material.uniforms.scale.value = this.fire.scale;
-
-    //this.el.setAttribute('scale', this.fire.scale);
-
-    //console.log(this.el.getAttribute('scale'));
-  }
-});
-
-AFRAME.registerShader('fireShader', {
-  schema: {
-    /*luminance: {default: 1, max: 0, min: 2, is: 'uniform'},
-    mieCoefficient: {default: 0.005, min: 0, max: 0.1, is: 'uniform'},
-    mieDirectionalG: {default: 0.8, min: 0, max: 1, is: 'uniform'},
-    reileigh: {default: 1, max: 0, min: 4, is: 'uniform'},
-    sunPosition: {type: 'vec3', default: '0 0 -1', is: 'uniform'},
-    turbidity: {default: 2, max: 0, min: 20, is: 'uniform'}*/
+    this.material.uniforms.time.value = time / 1000; //always needed for flame flicker
   },
-
-  defines: {
-      "ITERATIONS"    : "20",
-      "OCTIVES"       : "3"
-  },
-
-  uniforms: {
-      "fireTex"       : { type : "t",     value : null },
-      "color"         : { type : "c",     value : null },
-      "time"          : { type : "f",     value : 0.0 },
-      "seed"          : { type : "f",     value : 0.0 },
-      "invModelMatrix": { type : "m4",    value : null },
-      "scale"         : { type : "v3",    value : null },
-
-      "noiseScale"    : { type : "v4",    value : new THREE.Vector4(1, 2, 1, 0.3) },
-      "magnitude"     : { type : "f",     value : 1.3 },
-      "lacunarity"    : { type : "f",     value : 2.0 },
-      "gain"          : { type : "f",     value : 0.5 }
-  },
-  
   vertexShader: [
     "varying vec3 vWorldPos;",
     "void main() {",
-        "gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);",
-        "vWorldPos = (modelMatrix * vec4(position, 1.0)).xyz;",
+      "gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);",
+      "vWorldPos = (modelMatrix * vec4(position, 1.0)).xyz;",
     "}"
-  ].join("\n"),
+  ].join('\n'),
   fragmentShader: [
+    "uniform vec3 vCameraPosition;",
     "uniform vec3 color;",
     "uniform float time;",
     "uniform float seed;",
@@ -167,10 +310,6 @@ AFRAME.registerShader('fireShader', {
         "vec3 i1 = min(g.xyz, l.zxy);",
         "vec3 i2 = max(g.xyz, l.zxy);",
 
-        //   x0 = x0 - 0.0 + 0.0 * C.xxx;
-        //   x1 = x0 - i1  + 1.0 * C.xxx;
-        //   x2 = x0 - i2  + 2.0 * C.xxx;
-        //   x3 = x0 - 1.0 + 3.0 * C.xxx;
         "vec3 x1 = x0 - i1 + C.xxx;",
         "vec3 x2 = x0 - i2 + C.yyy;", // 2.0*C.x = 1/3 = C.y
         "vec3 x3 = x0 - D.yyy;",      // -1.0+3.0*C.x = -0.5 = -D.y
@@ -199,8 +338,6 @@ AFRAME.registerShader('fireShader', {
         "vec4 b0 = vec4(x.xy, y.xy);",
         "vec4 b1 = vec4(x.zw, y.zw);",
 
-        //vec4 s0 = vec4(lessThan(b0,0.0))*2.0 - 1.0;
-        //vec4 s1 = vec4(lessThan(b1,0.0))*2.0 - 1.0;
         "vec4 s0 = floor(b0) * 2.0 + 1.0;",
         "vec4 s1 = floor(b1) * 2.0 + 1.0;",
         "vec4 sh = -step(h, vec4(0.0));",
@@ -280,12 +417,9 @@ AFRAME.registerShader('fireShader', {
         "col.a = col.r;",
 
         "gl_FragColor = col;",
-    "}",
-  ].join("\n")
+    "}"
+  ].join('\n')
 });
-
-
-
 
 /*** CRATE EXAMPLE ***/
 AFRAME.registerComponent('crate', {
